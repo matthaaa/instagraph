8181a2f77d12ad88ff336563dfdd8976f2c48347
Matt Ha <matthewha1993@gmail.com>
2018-01-30 14:17:02 -0800

    Create Users table and UsersController and SessionController

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
+  get 'users/create'
+
+  get 'users/index'
+
+  get 'users/show'
+
+  get 'users/edit'
+
+  get 'users/update'
+
+  get 'users/destroy'
+
+  get 'session/new'
+
+  get 'session/create'
+
+  get 'session/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

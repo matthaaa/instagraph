7dfdb25ec4e5f91cec69e7dd951cdfd63f95cf01
Matt Ha <matthewha1993@gmail.com>
2018-01-30 15:20:43 -0800

    Create frontend directories and render basic text on browser

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,10 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
-  get 'users/create'
-
-  get 'users/index'
-
-  get 'users/show'
-
-  get 'users/edit'
-
-  get 'users/update'
-
-  get 'users/destroy'
-
-  get 'session/new'
-
-  get 'session/create'
-
-  get 'session/destroy'
+  root to: 'static_pages#root'
+  
+  namespace :api, defaults: {format: :json} do
+    resources :users
+    resource :session
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

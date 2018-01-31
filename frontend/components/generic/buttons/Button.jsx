dc9bc9183b2e6cd0a846836f80656a8c148ca0cf
Matt Ha <matthewha1993@gmail.com>
2018-01-31 13:35:29 -0800

    Set Up LoginView form view and create generic button component

---
+++ frontend/components/generic/buttons/Button.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {Button, Component} from 'react';
+
+class CustomButton extends Component {
+
+  // ==================================================
+  // Callbacks
+  // ==================================================
+
+  // ==================================================
+  // Render
+  // ==================================================
+
+  render() {
+    const {onPress, text} = this.props;
+
+    return (
+      <button onClick={onPress}>{text}</button>
+    );
+  }
+
+};
+
+export default CustomButton;
--- frontend/components/session_views/login_view.jsx
+++ frontend/components/session_views/login_view.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import {Component} from 'react';
-import PropTypes from 'prop-types';
+
+// Components
+import Button from '../generic/buttons/Button'
 
 class LoginView extends Component {
 
@@ -8,13 +10,48 @@ class LoginView extends Component {
   // Callbacks
   // ==================================================
 
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit() {
+
+  }
+
+
   // ==================================================
   // Render
   // ==================================================
 
+  renderUsernameInput() {
+    return (
+      <label>Username
+        <input type="text"/>
+      </label>
+    );
+  }
+
+  renderPasswordInput() {
+    return (
+      <label>Password
+        <input type="password"/>
+      </label>
+    );
+  }
+
   render() {
     return (
-      <h1>This is the login view.</h1>
+      <div>
+        <h1>This is the login view.</h1>
+        <form>
+          {this.renderUsernameInput()}
+          <br/>
+          {this.renderPasswordInput()}
+          <br/>
+          <Button text={"Log In"} onPress={this.handleSubmit} />
+        </form>
+      </div>
     );
   }
 

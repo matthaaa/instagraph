211a1b8862fa738fc2815911f9d5dbb98c466494
Matt Ha <matthewha1993@gmail.com>
2018-01-30 17:30:58 -0800

    Add methods to SessionsController

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -3,8 +3,25 @@ class Api::SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by_creds(
+      params[:user][:username],
+      params[:user][:password]
+    )
+
+    if user
+      sign_in(user)
+      redirect_to links_url
+    else
+      flash.now[:errors] = [
+        'Sorry, your password was incorrect. Please double-check ' +
+        'your password.'
+      ]
+      render :new
+    end
   end
 
   def destroy
+    sign_out
+    redirect_to new_session_url
   end
 end

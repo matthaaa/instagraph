ed0df3567bc96d33e14ae87ecd80c1e9f1a44ac6
Matt Ha <matthewha1993@gmail.com>
2018-02-01 13:55:46 -0800

    Create blank component for main logo

---
+++ frontend/components/generic/logos/main_logo.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Component} from 'react';
+
+
+class MainLogo extends Component {
+
+  // ==================================================
+  // Render
+  // ==================================================
+  render() {
+    return (
+    );
+  }
+
+};
+
+export default MainLogo;

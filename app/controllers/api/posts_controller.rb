fb4e23d178252740b2184afc2681f15139ca5b24
Matt Ha <matthewha1993@gmail.com>
2018-02-02 02:20:18 -0800

    Create API/Posts controller

---
+++ app/controllers/api/posts_controller.rb
@@ -0,0 +1,22 @@
+class Api::PostsController < ApplicationController
+  def add
+  end
+
+  def create
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+
+  def edit
+  end
+
+  def update
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,46 @@
 Rails.application.routes.draw do
+  namespace :api do
+    get 'posts/add'
+  end
+
+  namespace :api do
+    get 'posts/create'
+  end
+
+  namespace :api do
+    get 'posts/index'
+  end
+
+  namespace :api do
+    get 'posts/show'
+  end
+
+  namespace :api do
+    get 'posts/destroy'
+  end
+
+  namespace :api do
+    get 'posts/edit'
+  end
+
+  namespace :api do
+    get 'posts/update'
+  end
+
+  get 'posts/add'
+
+  get 'posts/create'
+
+  get 'posts/index'
+
+  get 'posts/show'
+
+  get 'posts/destroy'
+
+  get 'posts/edit'
+
+  get 'posts/update'
+
   root to: 'static_pages#root'
 
   namespace :api, defaults: {format: :json} do

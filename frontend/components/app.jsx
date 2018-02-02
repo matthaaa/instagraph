6461d716f1c9aa14a3c1446364453dd45a29c7eb
Matt Ha <matthewha1993@gmail.com>
2018-02-01 16:29:40 -0800

    Create header style file and fix naming convention on inconsistent scss files

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -3,14 +3,16 @@ import {Route, Switch} from 'react-router-dom';
 // import {AuthRoute} from '../util/route_util';
 
 // Components
+import MainHeader from './generic/main_header';
+import HomeFeedContainer from './logged_in_views/post_views/home_feed_container';
 import LoginViewContainer from './session_views/login_view_container';
 import SignupViewContainer from './user_views/signup_view_container';
-import HomeFeedContainer from './logged_in_views/post_views/home_feed_container';
 
 
 const App = ({children}) => (
   <div>
     <link href="https://fonts.googleapis.com/css?family=Dancing+Script|Indie+Flower|Pacifico|Quicksand:300,400,500,700" rel="stylesheet"/>
+    <MainHeader/>
     <section className="main-view">
       <Switch>
         <Route path="/signup" component={SignupViewContainer} />

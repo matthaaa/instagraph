995d6fdb45cb9bd5bfb75b8e9f19d0be1f5d3771
Matt Ha <matthewha1993@gmail.com>
2018-02-02 02:21:16 -0800

    Consolidate posts routes into resources :posts in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,50 +1,9 @@
 Rails.application.routes.draw do
-  namespace :api do
-    get 'posts/add'
-  end
-
-  namespace :api do
-    get 'posts/create'
-  end
-
-  namespace :api do
-    get 'posts/index'
-  end
-
-  namespace :api do
-    get 'posts/show'
-  end
-
-  namespace :api do
-    get 'posts/destroy'
-  end
-
-  namespace :api do
-    get 'posts/edit'
-  end
-
-  namespace :api do
-    get 'posts/update'
-  end
-
-  get 'posts/add'
-
-  get 'posts/create'
-
-  get 'posts/index'
-
-  get 'posts/show'
-
-  get 'posts/destroy'
-
-  get 'posts/edit'
-
-  get 'posts/update'
-
   root to: 'static_pages#root'
 
   namespace :api, defaults: {format: :json} do
     resources :users
+    resources :posts
     resource :session, only: [:create, :destroy]
   end
 

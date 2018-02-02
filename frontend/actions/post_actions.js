91d8e90d2890afb65272a319a83827f214b946da
Matt Ha <matthewha1993@gmail.com>
2018-02-02 09:23:58 -0800

    Create post actions file with requestPosts action

---
+++ frontend/actions/post_actions.js
@@ -0,0 +1,13 @@
+import * as PostAPIUtil from '../util/post_api_util';
+
+
+export const RECEIVE_POSTS = 'RECEIVE_POSTS';
+
+export const requestPosts = () => (
+  PostAPIUtil.fetchPosts().then(posts => dispatch(receivePosts(posts)))
+);
+
+const receivePosts = (posts) => ({
+  type: RECEIVE_POSTS,
+  users,
+})

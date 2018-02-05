64649d4c231f119459240fd53bf06d7a3afad6c8
Matt Ha <matthewha1993@gmail.com>
2018-02-04 18:39:40 -0800

    Revert routes to not be nested for users and posts

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,8 @@ Rails.application.routes.draw do
   root to: 'static_pages#root'
 
   namespace :api, defaults: {format: :json} do
-    resources :users do
-      resources :posts
-    end
+    resources :users 
+    resources :posts
     resource :session, only: [:create, :destroy]
   end
 

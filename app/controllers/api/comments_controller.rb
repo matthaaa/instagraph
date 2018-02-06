449fe7bf51be1d03532e08fa46da93b115277162
Matt Ha <matthewha1993@gmail.com>
2018-02-06 02:12:21 -0800

    Add methods to comments controller

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,8 +1,16 @@
 class Api::CommentsController < ApplicationController
   def new
+    @comment = Comment.new
   end
 
   def create
+    @comment = Comment.new(comment_params)
+
+    if @comment.save
+      render :show
+    else
+      render json: @comment.errors.full_messages, status: 400
+    end
   end
 
   def index
@@ -18,5 +26,12 @@ class Api::CommentsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(
+      :body, :user_id, :post_id
+    )
   end
 end

8bf683596d02033db9025bb7eaf7b9a87872a75d
Matt Ha <matthewha1993@gmail.com>
2018-02-06 11:19:25 -0800

    Render comment form  with currentUser prop and post prop

---
+++ frontend/components/logged_in_views/post_views/components/post_list/components/comments/forms/add_comment_form_container.jsx
@@ -0,0 +1,19 @@
+import {connect} from 'react-redux';
+import {withRouter} from 'react-router';
+
+// Components
+import AddCommentForm from './add_comment_form';
+
+
+const mapStateToProps = (state, ownProps) => ({
+  currentUser: state.session.currentUser,
+})
+
+const mapDispatchToProps = (dispatch, ownProps) => ({
+})
+
+
+export default withRouter(connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(AddCommentForm));
--- frontend/components/logged_in_views/post_views/components/post_list/post_list_item.jsx
+++ frontend/components/logged_in_views/post_views/components/post_list/post_list_item.jsx
@@ -4,6 +4,7 @@ import {Component} from 'react';
 // Components
 import PostListItemHeader from './components/post_list_item_header';
 import PostListItemActionContainer from './components/post_list_item_action_container';
+import AddCommentFormContainer from './components/comments/forms/add_comment_form_container';
 import CommentListView from './components/comments/components/comments_list_view';
 
 
@@ -53,9 +54,11 @@ class PostListItem extends Component {
   }
 
 
-  renderCommentForm() {
+  renderCommentForm(post) {
     return(
-      <div className="comment-form" ></div>
+      <div className="comment-form" >
+        <AddCommentFormContainer post={post} />
+      </div>
     );
   }
 
@@ -74,7 +77,7 @@ class PostListItem extends Component {
         {this.renderDescription(post)}
         <CommentListView comments={post.comments} />
         {this.renderTimestamp(post)}
-        {this.renderCommentForm()}
+        {this.renderCommentForm(post)}
       </div>
     )
   }

8e4ef89f52851809fe2fb5022e61bddf35d30c2b
Matt Ha <matthewha1993@gmail.com>
2018-02-06 02:04:08 -0800

    Create comments controller and delete erb fils

---
+++ app/controllers/api/comments_controller.rb
@@ -0,0 +1,22 @@
+class Api::CommentsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
     resources :users
     resources :posts do
       resources :likes, only: [:create, :destroy]
+      resources :comments
     end
   end
 

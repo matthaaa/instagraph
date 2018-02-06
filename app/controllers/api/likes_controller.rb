0a2b804fb821c2d8b0731cdd96978b8ca6186281
Matt Ha <matthewha1993@gmail.com>
2018-02-05 19:52:36 -0800

    Add create and destroy methods to Like model

--- app/controllers/api/likes_controller.rb
+++ app/controllers/api/likes_controller.rb
@@ -1,7 +1,31 @@
 class Api::LikesController < ApplicationController
   def create
+    @like = Like.new(like_params)
+
+    if @like.save!
+      @post = Post.find_by(post_id: params[:post_id])
+      render :show
+    else
+      render json: @like.errors.full_messages
+    end
   end
 
   def destroy
+    @like = Like.find_by(
+      post_id: like_params[:post_id],
+      user_id: like_params[:user_id],
+    )
+
+    if @like.destroy
+      @post = Post.find_by(post_id: params[:post_id])
+      render :show
+    else
+      render json: @like.errors.full_messages
+    end
+  end
+
+  private
+  def like_params
+    params.require(:like).permit(:user_id, :post_id)
   end
 end

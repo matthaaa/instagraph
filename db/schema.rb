dbe0877a3afe323f3dfd9f15cef0ce1473afd6ff
Matt Ha <matthewha1993@gmail.com>
2018-02-06 02:02:22 -0800

    Create and migrate comments table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180206031515) do
+ActiveRecord::Schema.define(version: 20180206100052) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string "body", null: false
+    t.integer "user_id", null: false
+    t.integer "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "likes", force: :cascade do |t|
     t.string "user_id", null: false

1a376bc42b1ba37d243ddacd18a340b2f2908f88
Matt Ha <matthewha1993@gmail.com>
2018-02-05 18:53:58 -0800

    Add icon and handler for like and comment buttons

--- frontend/components/logged_in_views/post_views/components/post_list/components/post_list_item_actions.jsx
+++ frontend/components/logged_in_views/post_views/components/post_list/components/post_list_item_actions.jsx
@@ -7,10 +7,28 @@ class PostListItemActions extends Component {
   // ==================================================
   // Initialize
   // ==================================================
+  constructor(props) {
+    super(props)
+    this.handleLike = this.handleLike.bind(this);
+    this.handleComment = this.handleComment.bind(this);
+  }
 
   // ==================================================
   // Callbacks
   // ==================================================
+
+  // ==================================================
+  // Event Handlers
+  // ==================================================
+  handleLike() {
+    // TODO: Like action goes here
+    console.log("Like!");
+  }
+
+  handleComment() {
+    // TODO: Comment action goes here
+    console.log("Comment!");
+  }
 
   // ==================================================
   // Render
@@ -19,8 +37,20 @@ class PostListItemActions extends Component {
     // TODO: Add icons to buttons.
     return(
       <div className="post-action-bar" >
-        <button className="post-action-button post-like-button"></button>
-        <button className="post-action-button post-comment-button"></button>
+        <button
+          onClick={this.handleLike}
+          className="post-action-button post-like-button">
+          <img
+            src="https://s3-us-west-2.amazonaws.com/instagraph-aa/icon-heart.png"
+          />
+        </button>
+        <button
+          onClick={this.handleComment}
+          className="post-action-button post-comment-button">
+          <img
+            src="https://s3-us-west-2.amazonaws.com/instagraph-aa/icon-comment.png"
+          />
+        </button>
       </div>
     );
   }

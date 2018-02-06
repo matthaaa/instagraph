8b0dc9b47f309c7a17d1dc369314247a7c16b9e5
Matt Ha <matthewha1993@gmail.com>
2018-02-06 13:52:43 -0800

    Fix Comment actions and add thunk actions

--- frontend/actions/comment_actions.js
+++ frontend/actions/comment_actions.js
@@ -5,9 +5,19 @@ import {updatePost} from './post_actions';
 export const requestComment = (comment) => dispatch => {
   console.log(comment);
   return (
-  CommentAPIUtil.fetchComment(comment).then(comment => dispatch(updatePost(comment.post)))
+  CommentAPIUtil.fetchComment(comment).then(comment => dispatch(receiveComment(comment)))
 )};
 
-export const deleteLike = (comment) => dispatch  => (
-  CommentAPIUtil.destroyComment(comment).then(comment => dispatch(updatePost(comment.post)))
+export const deleteComment = (comment) => dispatch  => (
+  CommentAPIUtil.destroyComment(comment).then(comment => dispatch(removeComment(comment)))
 );
+
+const receiveComment = (comment) => ({
+  type: RECEIVE_COMMENT,
+  comment,
+})
+
+const removeComment = (comment) => ({
+  type: REMOVE_COMMENT,
+  comment,
+})

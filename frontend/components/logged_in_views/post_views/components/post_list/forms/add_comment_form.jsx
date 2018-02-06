79a6857b3056f83d48918fb2688de4d8e807a88c
Matt Ha <matthewha1993@gmail.com>
2018-02-06 10:49:45 -0800

    Create comment form

--- frontend/components/logged_in_views/post_views/components/post_list/forms/add_comment_form.jsx
+++ frontend/components/logged_in_views/post_views/components/post_list/forms/add_comment_form.jsx
@@ -1,22 +1,52 @@
 import React from 'react';
 import {Component} from 'react';
 
+// Components
+import FormInput from 'frontend/components/generic/forms/form_input';
+
 
 class AddCommentForm extends Component {
 
   // ==================================================
   // Initialize
   // ==================================================
+  constructor(props) {
+    super(props);
+    this.state = {
+      comment: {
+        body: "",
+        user_id: this.props.currentUser.id,
+      }
+    };
+  }
 
   // ==================================================
-  // Callbacks
+  // Event Handlers
   // ==================================================
+  handleUpdate(field) {
+    return (event) => {
+      this.setState({
+        comment: Object.assign(this.state.comment, {
+          body: event.target.value,
+        })
+      });
+    }
+  }
 
   // ==================================================
   // Render
   // ==================================================
   render() {
     return (
+      <form className="new-comment-form-content">
+        <FormInput
+          type="textarea"
+          contentClassName="new-comment-form-input-container"
+          placeholder={"Add a comment..."}
+          value={this.state.comment.body}
+          onChange={this.handleUpdate}
+        />
+      </form>
     );
   }
 

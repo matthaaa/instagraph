acb7aa9780a9c87b779a4f33b06090d7db70d0a1
Matt Ha <matthewha1993@gmail.com>
2018-02-06 15:44:30 -0800

    Clean up routes for followds

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,11 @@
 Rails.application.routes.draw do
-  namespace :api do
-    get 'follows/create'
-  end
-
-  namespace :api do
-    get 'follows/destroy'
-  end
-
   root to: 'static_pages#root'
 
   namespace :api, defaults: {format: :json} do
     resource :session, only: [:create, :destroy]
     resources :users
     resources :comments, only: [:index, :create, :destroy]
+    resources :follows, only: [:create, :destroy]
     resources :posts do
       resources :likes, only: [:create, :destroy]
     end

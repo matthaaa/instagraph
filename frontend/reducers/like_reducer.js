af6615903a7f7e316e5ffaaac500f1955b0f6c87
Matt Ha <matthewha1993@gmail.com>
2018-02-08 03:19:23 -0800

    Fix imports on like_reducer

--- frontend/reducers/like_reducer.js
+++ frontend/reducers/like_reducer.js
@@ -1,7 +1,5 @@
 import {merge} from 'lodash';
-
 import {RECEIVE_POST} from '../actions/post_actions';
-import {RECEIVE_USER} from '../actions/user_actions';
 
 const likeReducer = (oldState = {}, action) => {
   Object.freeze(oldState);

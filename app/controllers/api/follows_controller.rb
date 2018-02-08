57c05e172efbe0b1379daf26a5c7c43a86643139
Matt Ha <matthewha1993@gmail.com>
2018-02-08 01:22:26 -0800

    Add destroy logic for follow

--- app/controllers/api/follows_controller.rb
+++ app/controllers/api/follows_controller.rb
@@ -13,6 +13,18 @@ class Api::FollowsController < ApplicationController
   end
 
   def destroy
+    @follow = Follow.find_by(
+      follower_id: like_params[:follower_id],
+      followed_id: like_params[:followed_id],
+    )
+    # @like = Like.find_by(id: params[:id])
+    @user = @like.follower
+
+    if @follow.destroy
+      render 'api/users/show'
+    else
+      render json: @follow.errors.full_messages
+    end
   end
 
   private

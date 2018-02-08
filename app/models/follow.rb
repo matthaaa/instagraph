344d850d5d470e77c27b606732963262a001aad0
Matt Ha <matthewha1993@gmail.com>
2018-02-08 01:17:30 -0800

    Create add method logic in follows controller

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -2,14 +2,14 @@ class Follow < ApplicationRecord
   validates :follower_id :followed_id, presence: true
 
   # User who is following
-  belongs_to :user,
+  belongs_to :follower,
     primary_key: :id,
     foreign_key: :follower_id,
-    class_name: :Follow
+    class_name: :User
 
   # Users who are being followed by follower_id user
-  has_many :users,
+  has_many :followees,
     primary_key: :id,
     foreign_key: :followed_id,
-    class_name: :Follow
+    class_name: :User
 end

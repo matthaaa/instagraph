5b68c4b04d9cb17341dfb1d67abbdf4c54348327
Matt Ha <matthewha1993@gmail.com>
2018-03-22 00:51:17 -0700

    Add name to bio section

--- frontend/components/user_views/components/user_profile_header.jsx
+++ frontend/components/user_views/components/user_profile_header.jsx
@@ -131,8 +131,9 @@ class UserProfileHeader extends Component {
 
   renderBio(user) {
     return (
-      <div className="main-profile-user-bio">
-        {user.bio}
+      <div className="main-profile-bio-container">
+        <p className="main-profile-user-bio-name">{user.username}</p>
+        <p className="main-profile-user-bio">{user.bio}</p>
       </div>
     );
   }

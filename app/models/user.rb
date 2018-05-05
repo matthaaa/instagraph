d5ce2f147d9d77571f49ccfe71fac9cad9575185
Matt <matthewha1993@gmail.com>
2018-05-05 15:25:04 -0700

    Validate bio in model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates :username, :email, :full_name, presence: true
+  validates :username, :email, :full_name, :bio, presence: true
   validates :password_digest, :session_token, presence: true
   validates :password, length: {minimum: 6, allow_nil: true}
 
